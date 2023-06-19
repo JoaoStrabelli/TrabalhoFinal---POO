3f593f58b43a5304d84aa2bec2fd0e9261c996a1
Fellyipe <felipebueno2004@hotmail.com>
2023-06-19 15:21:22 -0300

    Alterações 5001

--- TrabalhoFinal/Models/Categoria.cs
+++ TrabalhoFinal/Models/Categoria.cs
@@ -2,7 +2,16 @@ namespace SalesManagement
 {
     public class CategoriaModel
     {
-        public long? CategoriaID { get; set; }
-        public string Nome { get; set; } = string.Empty;
+        private long _id;
+        public string _nome;
+        
+        public int Id{
+            get { return _id; }
+            set { _id = value; }
+        }
+        public int Nome{
+            get { return _nome; }
+            set { _nome = value; }
+        }
     }
 }
