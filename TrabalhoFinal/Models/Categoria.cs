e6a4db2bb2d84d44182882b361798a3a6c313b0e
Fellyipe <felipebueno2004@hotmail.com>
2023-06-19 15:24:27 -0300

    ,,,,

--- TrabalhoFinal/Models/Categoria.cs
+++ TrabalhoFinal/Models/Categoria.cs
@@ -5,11 +5,11 @@ namespace SalesManagement
         private long _id;
         public string _nome;
         
-        public int Id{
+        public long Id{
             get { return _id; }
             set { _id = value; }
         }
-        public int Nome{
+        public string Nome{
             get { return _nome; }
             set { _nome = value; }
         }
