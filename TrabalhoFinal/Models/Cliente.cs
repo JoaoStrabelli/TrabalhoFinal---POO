5507434b53bd247177b1003e838f0b96496da790
Fellyipe <felipebueno2004@hotmail.com>
2023-06-19 15:50:06 -0300

    ..

--- TrabalhoFinal/Models/Cliente.cs
+++ TrabalhoFinal/Models/Cliente.cs
@@ -2,18 +2,37 @@ namespace SalesManagement
 {
     public class Cliente
     {
-        public long? Id { get; set; }
-        public string Nome { get; set; } = string.Empty;
-        public string Sobrenome { get; set; } = string.Empty;
-        public string Endereco { get; set; } = string.Empty;
-        public string Telefone { get; set; } = string.Empty;
+        private long _id;
+        private string _nome = "";
+        private string _sobrenome = "";
+        private string _endereco = "";
+        private string _telefone = "";
 
-        public string NomeCompleto
+        public string NomeCompleto()
         {
-            get
-            {
-                return $"{Nome} {Sobrenome}";
-            }
+            return $"{Nome} {Sobrenome}";
         }
+
+        public long Id{
+            get { return _id; }
+            set { _id = value; }
+        }
+        public long Nome{
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        public long Sobrenome{
+            get { return _sobrenome; }
+            set { _sobrenome = value; }
+        }
+        public string Endereco{
+            get { return _endereco; }
+            set { _endereco = value; }
+        }
+        public string Telefone{
+            get { return _telefone; }
+            set { _telefone = value; }
+        }
+
     }
 }
